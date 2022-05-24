b97ebb3bc36e6d5b13d7e15af560030afbb2aeff
Gautam <gautamkhaj07@gmail.com>
2022-05-23 20:16:35 -0700

    root

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+// wow its a next app
 export default function Home() {
   return (
     <div className={styles.container}>
