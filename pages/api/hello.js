f3461d428b374ca36180278fed2eaa119fbbcc6f
Sptele <gautamkhaj07@gmail.com>
2022-05-25 11:45:14 -0700

    inital commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+	const data = {
+		name: "Gautam Khajuria",
+		pronouns: ["he", "him", "his"],
+		github: "https://github.com/Sptele",
+		profilePic: "https://avatars.githubusercontent.com/u/79765399?v=4",
+		occupation: "Student",
+		programmingLangs: ["Java", "JavaScript", "C++", "C#", "HTML", "CSS", "Python"],
+		preferences: {
+			IDES: {
+				Java: "IntelliJ IDEA",
+				CPP: "Visual Studio Code",
+				CSharp: "Visual Studio",
+				JavaScript: "Visual Studio Code",
+				HTMLCSS: "Visual Studio Code",
+				Python: "Pystorm"
+			},
+			computer: "Some ASUS lol",
+
+		}
+	}
+	res.status(200).json(data);
 }
